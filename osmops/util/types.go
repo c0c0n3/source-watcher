1beedd11394728c5e21a7b2a4dd4040304f17627
Andrea Falconi <andrea.falconi@gmail.com>
2021-08-16 14:31:53 +0200

    implement convenience fn to join abs paths and test if an abs path is a dir.

--- osmops/util/types.go
+++ osmops/util/types.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"os"
 	"path/filepath"
 	"regexp"
 	"strconv"
@@ -35,6 +36,32 @@ func ParseAbsPath(path string) (AbsPath, error) {
 	}
 
 	// (*) Abs doesn't trim space, e.g. Abs('/a/b ') == '/a/b '.
+}
+
+func (d AbsPath) Join(relativePath string) AbsPath {
+	rest := strings.TrimSpace(relativePath) // (1)
+	return AbsPath{
+		data: filepath.Join(d.Value(), rest), // (2)
+	}
+
+	// (1) Join doesn't trim space, e.g. Join("/a", "/b ") == "/a/b "
+	// (2) In principle this is wrong since we don't know if relativePath
+	// is a valid path according to the FS we're running on. (Join doesn't
+	// check that.) So we could potentially return an inconsistent AbsPath.
+	// Go's standard lib is quite weak in the handling of abstract paths,
+	// i.e. independent of OS, so this is the best we can do. See e.g.
+	// - https://stackoverflow.com/questions/35231846
+}
+
+func (d AbsPath) IsDir() error {
+	if f, err := os.Stat(d.Value()); err != nil {
+		return err
+	} else {
+		if !f.IsDir() {
+			return fmt.Errorf("not a directory: %v", d.Value())
+		}
+	}
+	return nil
 }
 
 type NonEmptyStr struct{ data string }
--- osmops/util/types_test.go
+++ osmops/util/types_test.go
@@ -2,9 +2,15 @@ package util
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 )
+
+// TODO. The path tests will probably fail on Windows since we're using
+// Unix paths. We could use filepath.Join to make most of them platform
+// independent but I'm not sure how to make absolute paths though...
 
 var invalidPathFixtures = []string{"", " ", "\n", "\t "}
 
@@ -34,6 +40,57 @@ func TestParsePath(t *testing.T) {
 			}
 			if d.rel && !strings.HasSuffix(p.Value(), d.want) {
 				t.Errorf("[%d] want suffix: %s; got: %s", k, d.want, p.Value())
+			}
+		}
+	}
+}
+
+var joinPathFixtures = []struct {
+	base string
+	rel  string
+	want string
+}{
+	{"/a", "", "/a"}, {"/a/", " ", "/a"}, {"/a", "\t", "/a"},
+	{"/a/", "b ", "/a/b"}, {"/a", "b\n", "/a/b"}, {"/a/b", "//c", "/a/b/c"},
+}
+
+func TestJoinPath(t *testing.T) {
+	for k, d := range joinPathFixtures {
+		if base, err := ParseAbsPath(d.base); err != nil {
+			t.Errorf("[%d] want: valid parse; got: %v", k, err)
+		} else {
+			joined := base.Join(d.rel)
+			if joined.Value() != d.want {
+				t.Errorf("[%d] want: %s; got: %s", k, d.want, joined)
+			}
+		}
+	}
+}
+
+func TestIsDir(t *testing.T) {
+	if pwd, err := ParseAbsPath("."); err != nil {
+		t.Errorf("want: valid parse; got: %v", err)
+	} else {
+		if err := pwd.IsDir(); err != nil {
+			t.Errorf("want: pwd is a directory; got: %v", err)
+		}
+
+		notThere := pwd.Join("notThere")
+		if err := notThere.IsDir(); err == nil {
+			t.Errorf("want: not a directory; got directory: %v", notThere)
+		}
+
+		if tempFile, err := ioutil.TempFile("", "prefix"); err != nil {
+			t.Errorf("couldn't create temp file: %v", err)
+		} else {
+			defer os.Remove(tempFile.Name())
+
+			if tf, err := ParseAbsPath(tempFile.Name()); err != nil {
+				t.Errorf("want: valid temp file parse; got: %v", err)
+			} else {
+				if err := tf.IsDir(); err == nil {
+					t.Errorf("want: not a dir; got dir: %v", tf)
+				}
 			}
 		}
 	}
