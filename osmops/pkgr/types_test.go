bd2ba88e60056a77df92f691dd690b4c406c8c90
Andrea Falconi <andrea.falconi@gmail.com>
2022-06-13 16:57:09 +0200

    add method to get all pkg tgz data in one go.

--- osmops/pkgr/types_test.go
+++ osmops/pkgr/types_test.go
@@ -1,8 +1,10 @@
 package pkgr
 
 import (
+	"reflect"
 	"testing"
 
+	"github.com/fluxcd/source-watcher/osmops/util/bytez"
 	"github.com/fluxcd/source-watcher/osmops/util/file"
 )
 
@@ -12,5 +14,19 @@ func TestFileHashFailedLookup(t *testing.T) {
 	got := pkgSrc.FileHash("not/there")
 	if got != "" {
 		t.Errorf("want: empty; got: %s", got)
+	}
+}
+
+func TestReadAllData(t *testing.T) {
+	srcDir, _ := file.ParseAbsPath("no/where")
+	src := newPkgSrc(srcDir)
+	data := bytez.NewBuffer()
+	data.Write([]byte{1, 2, 3})
+	pkg := makePackage(src, data)
+
+	want := []byte{1, 2, 3}
+	got := pkg.ReadAllData()
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("want: %v; got: %v", want, got)
 	}
 }
